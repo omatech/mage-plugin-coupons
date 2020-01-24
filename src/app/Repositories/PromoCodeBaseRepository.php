2cb52c22876b7093987a624c8340017726796d63
victorqm98 <45096513+victorqm98@users.noreply.github.com>
2020-01-24 13:27:06 +0100

    Update PromoCodeBaseRepository Lars

--- src/app/Repositories/PromoCodeBaseRepository.php
+++ src/app/Repositories/PromoCodeBaseRepository.php
@@ -2,7 +2,7 @@
 
 namespace Omatech\MagePluginCoupons\App\Repositories;
 
-use Omatech\Mage\App\Repositories\BaseRepository;
+use Omatech\Lars\BaseRepository;
 use Omatech\LaravelPromoCodes\Models\PromoCode;
 
 abstract class PromoCodeBaseRepository extends BaseRepository
